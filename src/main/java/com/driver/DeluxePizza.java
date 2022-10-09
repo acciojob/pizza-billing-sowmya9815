45d0b2990d9fffbb2cc31ff65acd0c681c235a68
sowmya9815 <sowmyaeedala9815@gmail.com>
2022-10-09 17:22:38 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,7 +5,12 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        addExtraCheese();
-        addExtraToppings();
+        if(isVeg){
+            total+=70;
+        }
+        else{
+            total+=120;
+        }
+        total+=80;
     }
 }
